c4e942ebfd73a511cabc40c2d74ae3fa763c7a8f
sliberman <teclado3>
2020-02-02 19:10:30 +0100

    PF working

--- association/ml_association.py
+++ association/ml_association.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
+from math import pi
+
 import numpy as np
 
 
-class MLAssociation:
+class MLKalmanAssociation:
     def __init__(self, R, H, threshold):
         self.mu_bar = None
         self.Sigma_bar = None
@@ -14,7 +15,7 @@ class MLAssociation:
         self.mu_bar = mu_bar
         self.Sigma_bar = Sigma_bar
 
-    def associate(self, img, measurements):
+    def associate(self, measurements):
         """Associate one of the measurements (objects detected) to the predicted
         position.
 
@@ -42,3 +43,53 @@ class MLAssociation:
             return measurements[c]
         else:
             return None
+
+
+class MLPFAssociation:
+    def __init__(self, states, R, H, threshold):
+        self.S_bar = states
+        self.R = R
+        self.Rinv = np.linalg.pinv(R)
+        self.eta = 1.0 / ((2 * pi * np.linalg.det(R)) ** 0.5)
+        self.threshold = threshold
+        self.M = len(states)
+        self.H = H
+
+    def associate(self, measurements):
+        """Associate one of the measurements (objects detected) to the predicted
+        position.
+
+        Do an outlier check if the mahalonobis distance is higher than the
+        threshold. If it is, return the feature. Else, return None.
+
+        :param measurements: Array of measured features
+        :return position of the measured object (x, y) or None
+        """
+        n = len(measurements)
+        exps = np.zeros((self.M, n))
+        Psi = np.zeros((self.M, n))
+        c = np.zeros((self.M, n))
+
+        # Observation model
+        z_hat = self.observation_model()
+
+        # Difference
+        nu = z_hat.reshape(-1, 1, 2) - measurements.reshape(1, -1, 2)
+
+        # Calculate Mahalonobis distance
+        for i in range(n):
+            for m in range(self.M):
+                cur_nu = nu[m, i]
+                exps[m, i] = cur_nu.reshape(1, -1).dot(self.Rinv).dot(cur_nu.reshape(-1, 1))
+
+        psi = self.eta * np.exp(-0.5 * exps)
+
+        outlier = psi.mean(axis=0) < self.threshold
+
+        c = np.argmax(psi, axis=1)
+
+        return psi, outlier, c
+
+    def observation_model(self):
+        z_hat = self.S_bar.dot(self.H.T)
+        return z_hat

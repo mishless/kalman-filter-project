4bf96cc6a7368af764f60b26bfc6e8d6ff08bf7a
sliberman <teclado3>
2020-02-08 15:45:18 +0100

    Fixed initial state, added more features and experiments are working

--- particle_filter.py
+++ particle_filter.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 class ParticleFilter:
-    def __init__(self, num_particles, Q=None, R=None, img_shape=(100, 100), mean_speed=1, resample_mode="systematic"):
+    def __init__(self, num_particles, Q=None, R=None, img_shape=(100, 100), mean_speed=10, resample_mode="systematic", initial_state=None):
         self.M = num_particles
         self.S = np.empty((num_particles, 4))
 
         # Initialize states
-        self.initialize_states(img_shape, mean_speed)
+        self.initialize_states(img_shape, mean_speed, initial_state)
 
         self.S_bar = np.copy(self.S)
         self.weights = np.ones(num_particles) / num_particles
@@ -73,11 +73,19 @@ class ParticleFilter:
                 self.weights[mask] *= psi[mask, i]
         self.weights /= self.weights.sum()
 
-    def initialize_states(self, img_shape, max_speed):
-        s = np.random.uniform(0, 1, size=(len(self.S), 2))
-        self.S[:, 0] = s[:, 0] * img_shape[1]  # x
+    def initialize_states(self, img_shape, max_speed, initial_state):
+        if initial_state is not None:
+            # Global tracking
+            s = np.random.randn(len(self.S), 2) * 25
+            self.S[:, 0] = s[:, 0] + initial_state[0]  # x
+            self.S[:, 2] = s[:, 1] + initial_state[1]  # y
+        else:
+            # Local tracking
+            s = np.random.uniform(0, 1, size=(len(self.S), 2))
+            self.S[:, 0] = s[:, 0] * img_shape[1]  # x
+            self.S[:, 2] = s[:, 1] * img_shape[0]  # y
+
         self.S[:, 1] = max_speed * np.random.randn(len(self.S))  # vx
-        self.S[:, 2] = s[:, 1] * img_shape[0]  # y
         self.S[:, 3] = max_speed * np.random.randn(len(self.S))  # vy
 
     def systematic_resample(self):
--- track_pf.py
+++ track_pf.py
@@ -22,8 +22,8 @@ def main():
     diff = 0
 
     # Get images list from dataset
-    dataset = "data/jogging"
-    ground_truth_file = dataset + "/groundtruth_rect.1.txt"
+    dataset = "data/Car4"
+    ground_truth_file = dataset + "/groundtruth_rect.txt"
     images_wildcard = dataset + "/img/*.jpg"
     images_filelist = glob(images_wildcard)
 
@@ -37,41 +37,39 @@ def main():
 
     initial_position = ground_truth[0]
     x, y, w, h = initial_position
-    x = int(x)
-    y = int(y)
+    x = int(float(x) + float(w)/2)
+    y = int(float(y) + float(h)/2)
     w = int(w)
     h = int(h)
 
-    resample_every = 1
-    plot_particles = False
-    plot_features = True
-    extract_density = True
-    plot_density = True
+    resample_every: int = 1  # n: resample once every n frames
+    plot_particles: bool = False  # If true, draw green dots on the particles positions
+    plot_ground_truth: bool = True  # If true, plot a yellow bounding box on the tracked object
+    plot_detector_output: bool = False  # If true, plot a blue bounding box on the output of the detector
+    use_em_to_find_q: bool = False  # If true, use EM to estimate the Q matrix (experimental)
+    global_tracking: bool = True  # If true, use the first ground truth datum to initialize the estimator on the target as a prior.
+    extract_density: bool = True  # If true, extract density of the results. True required for plot_density and plot_detected_mean.
+    plot_density: bool = True  # to be used with extract density
+    plot_detected_mean: bool = True  # to be used with extract density
 
-    # img = plt.imread(images_filelist[0])
-    # plt.imshow(img)
-    # r = mpatches.Rectangle((x, y), w, h, linewidth=1, facecolor="none", edgecolor="red")
-    # ax = plt.gca()
-    # ax.add_patch(r)
-    # plt.show()
-
-    Q = 0.05 * np.eye(4)
     R = 750 * np.eye(2)
     # Initialize KF (x = [x, vx, y, vy])
     M = 1000
-    pf = ParticleFilter(num_particles=M, R=R, Q=Q, img_shape=img.shape, resample_mode="multinomial")
+    pf = ParticleFilter(num_particles=M, R=R, img_shape=img.shape, resample_mode="multinomial", initial_state=(x, y))
+
+    if not global_tracking:
+        pf.S[:, :2] = np.random.randn()
 
     # Initialize features detector
     fd = FeaturesDetector()
 
     # Find Q using EM
-    # em_obj = EM()
-    # data = np.array(ground_truth)[:, :2].astype(int)
-    # likelihoods = em_obj.find_Q(data)
-    # print(em_obj.kf.Q)
-    # pf.Q = em_obj.kf.Q.astype(int)*0.01
-    # plt.plot(likelihoods)
-    # plt.show()
+    if use_em_to_find_q:
+        em_obj = EM()
+        data = np.array(ground_truth)[:, :2].astype(int)
+        pf.Q = em_obj.kf.Q.astype(int)*0.01
+    else:
+        pf.Q = 0.05 * np.eye(4)
 
     # Iterate of every image
     features = {}
@@ -79,16 +77,17 @@ def main():
 
     da = DataAssociation(states=pf.S, R=pf.R, H=pf.H, threshold=1e-5)
 
-    # Plot initialized states
-    # x = pf.get_x().T
-    # plt.gca()
-    # plt.cla()
-    # plt.imshow(img)
-    # plt.plot(x[0].astype(int), x[1].astype(int), 'g.')
-    # plt.pause(0.1)
-
     fig, ax = plt.subplots(1, 1)
     plt.ion()
+
+    # Define the borders
+    xmin = 0
+    xmax = img.shape[1]
+    ymin = 0
+    ymax = img.shape[0]
+    # Create meshgrid
+    xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+
     for i, im in enumerate(t):
         img = plt.imread(images_filelist[i])
         plt.gca()
@@ -98,28 +97,17 @@ def main():
         # Do prediction
         pf.predict()
 
-        # Plot predicted
-        # plt.gca()
-        # plt.cla()
-        # plt.imshow(img)
-        # x = pf.S_bar[:, [0, 2]].T
-        # plt.plot(x[0].astype(int), x[1].astype(int), 'g.')
-        # plt.xlim(0, img.shape[1])
-        # plt.ylim(0, img.shape[0])
-        # plt.pause(0.1)
-
         if i % resample_every == 0:
 
             # Compute features
             features[i] = np.array(fd.compute_features(im))
 
             # Plot features
-            if plot_features:
+            if plot_detector_output:
                 for f in features[i]:
                     r = mpatches.Rectangle((f[0] - w/2, f[1] - h/2), w, h, linewidth=1, linestyle="solid",
                                            edgecolor="r", facecolor=None, fill=None)
                     ax.add_patch(r)
-
 
             # Do data association
             psi, outlier, c = da.associate(features[i])
@@ -129,45 +117,36 @@ def main():
         else:
             pf.assign_predicted()
 
-        # gt = list(map(int, ground_truth[i]))
-
+        gt = list(map(int, ground_truth[i]))
+        if plot_ground_truth:
+            r = mpatches.Rectangle((gt[0], gt[1]), gt[2], gt[3], linewidth=1, linestyle="solid",
+                                   edgecolor="y", facecolor=None, fill=None)
+            ax.add_patch(r)
         x = pf.get_x().T
         plt.gca().autoscale(False)
         if extract_density:
-            # Extract x and y
-            X = x[:, 0]
-            Y = x[:, 1]
-            # Define the borders
-            deltaX = (max(X) - min(X)) / 10
-            deltaY = (max(Y) - min(Y)) / 10
-            xmin = 0
-            xmax = img.shape[1]
-            ymin = 0
-            ymax = img.shape[0]
-            # Create meshgrid
-            xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
-
             kernel = st.gaussian_kde(x)
             positions = np.vstack([xx.ravel(), yy.ravel()])
             f = np.reshape(kernel(positions).T, xx.shape)
+            imean = np.argmax(f)
+            xmean = int(round(xx[imean // 100, imean % 100]))
+            ymean = int(round(yy[imean // 100, imean % 100]))
         if plot_particles:
             plt.plot(x[0].astype(int), x[1].astype(int), 'g.')
         if plot_density and extract_density:
             f[f < 1e-5] = np.nan
             cfset = ax.contourf(xx, yy, f, cmap='coolwarm', alpha=0.5)
-            # ax.imshow(np.rot90(f), cmap='coolwarm')
             cset = ax.contour(xx, yy, f, colors='k', alpha=0.5)
-            # ax.clabel(cset, inline=1, fontsize=10)
-            # ax.set_xlabel('X')
-            # ax.set_ylabel('Y')
-            # plt.title('2D Gaussian Kernel density estimation')
+        if plot_detected_mean and extract_density:
+            r = mpatches.Rectangle((xmean - w/2, ymean - h/2), gt[2], gt[3], linewidth=1, linestyle="solid",
+                                   edgecolor="b", facecolor=None, fill=None)
+            ax.add_patch(r)
+            v = np.linalg.norm([xmean - gt[0] - w/2, ymean - gt[1] - h/2], axis=0)
+            t.set_description(f"pred_pos=({xmean:3}, {ymean:3}) | gt_pos=({round(gt[0] + w/2):3}, {round(gt[1] + h/2):3}) | l2_dist={v:2.4}")
+            diff += v
         plt.pause(0.1)
 
-        # print(
-        #     f"Predicted position: {pf.x[0][0], pf.x[2][0]}, Ground truth position: {gt[0] + w/2, gt[1] + h/2}")
-        # diff += np.linalg.norm([pf.x[0][0] - gt[0] - w/2, pf.x[2][0] - gt[1] - h/2], axis=0)
-
-    print(diff / len(images_filelist))
+    print("Mean euclidean distance:", diff / len(images_filelist))
 
 
 if __name__ == '__main__':

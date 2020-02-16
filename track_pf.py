9846c4f9721d14d97262660b0506ac9bb084d908
Sergio Liberman Bronfman <sergiolib@gmail.com>
2020-02-16 17:21:33 +0100

    Changed the pf test file to be the official one. Can operate in point and box estimate modes

--- track_pf.py
+++ track_pf.py
@@ -1,22 +1,51 @@
+import scipy.stats as st
+from yolo.yolo_object_detection import YOLOObjectDetection as FeaturesDetector
+from particle_filter import ParticleFilter
+from association.ml_association import MLPFAssociation as DataAssociation
+from tqdm import tqdm
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import numpy as np
+from random import sample
+from glob import glob
 import csv
-
 import matplotlib
-
 from expectation_maximisation import EM
 
 matplotlib.use("Qt5Cairo")
-from glob import glob
 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from tqdm import tqdm
+# Parameters
+resample_every: int = 1  # n: resample once every n frames
+plot_particles: bool = True  # If true, draw green dots on the particles positions
+# If true, plot a yellow bounding box on the tracked object
+plot_ground_truth: bool = False
+# If true, plot a blue bounding box on the output of the detector
+plot_detector_output: bool = False
+# If true, use EM to estimate the Q matrix (experimental)
+use_em_to_find_q: bool = False
+# If true, use the first ground truth datum to initialize the estimator on the target as a prior.
+global_tracking: bool = False
+# If true, extract density of the results. True required for plot_density and plot_detected_mean.
+extract_density: bool = False
+plot_density: bool = False  # to be used with extract density
+plot_detected_mean: bool = False  # to be used with extract density
+resample_mode = "systematic"
 
-from association.ml_association import MLPFAssociation as DataAssociation
-from particle_filter import ParticleFilter
-from yolo.yolo_object_detection import YOLOObjectDetection as FeaturesDetector
+point_estimate = False
 
-import scipy.stats as st
+
+if point_estimate:
+    outlier_detector_threshold = 1e-6
+    num_particles = 1000
+    R = 750*np.eye(2)
+    Q = 0.01*np.eye(4)
+else:
+    outlier_detector_threshold = 1e-9
+    num_particles = 10000
+    R = 1500*np.eye(4)
+    Q = 0.01*np.eye(8)
+yolo_confidence = 0.1
+
 
 def main():
     diff = 0
@@ -37,45 +66,43 @@ def main():
 
     initial_position = ground_truth[0]
     x, y, w, h = initial_position
-    x = int(float(x) + float(w)/2)
-    y = int(float(y) + float(h)/2)
-    w = int(w)
-    h = int(h)
+    x = float(x)
+    y = float(y)
+    w = float(w)
+    h = float(h)
 
-    resample_every: int = 1  # n: resample once every n frames
-    plot_particles: bool = False  # If true, draw green dots on the particles positions
-    plot_ground_truth: bool = True  # If true, plot a yellow bounding box on the tracked object
-    plot_detector_output: bool = False  # If true, plot a blue bounding box on the output of the detector
-    use_em_to_find_q: bool = False  # If true, use EM to estimate the Q matrix (experimental)
-    global_tracking: bool = True  # If true, use the first ground truth datum to initialize the estimator on the target as a prior.
-    extract_density: bool = True  # If true, extract density of the results. True required for plot_density and plot_detected_mean.
-    plot_density: bool = True  # to be used with extract density
-    plot_detected_mean: bool = True  # to be used with extract density
-
-    R = 750 * np.eye(2)
-    # Initialize KF (x = [x, vx, y, vy])
-    M = 1000
-    pf = ParticleFilter(num_particles=M, R=R, img_shape=img.shape, resample_mode="multinomial", initial_state=(x, y))
-
-    if not global_tracking:
-        pf.S[:, :2] = np.random.randn()
+    # Initialize PF
+    initial_state = None
+    if point_estimate:
+        initial_state = (x+w/2, y+h/2)
+    else:
+        if not global_tracking:
+            initial_state = (x, y, x+w, y+h)
+    pf = ParticleFilter(num_particles=num_particles, R=R, img_shape=img.shape, resample_mode=resample_mode,
+                        initial_state=initial_state, point_estimate=point_estimate)
 
     # Initialize features detector
-    fd = FeaturesDetector()
+    fd = FeaturesDetector(confidence=yolo_confidence)
 
     # Find Q using EM
     if use_em_to_find_q:
         em_obj = EM()
-        data = np.array(ground_truth)[:, :2].astype(int)
-        pf.Q = em_obj.kf.Q.astype(int)*0.01
+        data = np.array(ground_truth)[:, :4].astype(int)
+        data[:, 2:] += data[:, :2]
+        pf.Q = em_obj.find_Q(data)
     else:
-        pf.Q = 0.05 * np.eye(4)
+        pf.Q = Q
 
     # Iterate of every image
     features = {}
-    t = tqdm(images_filelist[1:], desc="Processing")
+    if global_tracking:
+        start = 0
+    else:
+        start = 1
+    t = tqdm(images_filelist[start:], desc="Processing")
 
-    da = DataAssociation(states=pf.S, R=pf.R, H=pf.H, threshold=1e-5)
+    da = DataAssociation(states=pf.S, R=pf.R, H=pf.H,
+                         threshold=outlier_detector_threshold)
 
     fig, ax = plt.subplots(1, 1)
     plt.ion()
@@ -89,7 +116,7 @@ def main():
     xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
 
     for i, im in enumerate(t):
-        img = plt.imread(images_filelist[i])
+        img = plt.imread(im)
         plt.gca()
         plt.cla()
         plt.imshow(img)
@@ -102,14 +129,20 @@ def main():
             # Compute features
             features[i] = np.array(fd.compute_features(im))
 
+            # Filter out size <=0 features
+            features[i] = list(filter(lambda x: x[2]-x[0] >
+                                      0 and x[3]-x[1] > 0, features[i]))
+
             # Plot features
             if plot_detector_output:
                 for f in features[i]:
-                    r = mpatches.Rectangle((f[0] - w/2, f[1] - h/2), w, h, linewidth=1, linestyle="solid",
+                    r = mpatches.Rectangle((f[0], f[1]), f[2]-f[0], f[3]-f[1], linewidth=1, linestyle="solid",
                                            edgecolor="r", facecolor=None, fill=None)
                     ax.add_patch(r)
 
             # Do data association
+            if point_estimate:
+                features[i] = [np.array([i[0] + w/2, i[1] + h/2]) for i in features[i]]
             psi, outlier, c = da.associate(features[i])
 
             pf.update(psi, outlier, c)
@@ -119,20 +152,30 @@ def main():
 
         gt = list(map(int, ground_truth[i]))
         if plot_ground_truth:
-            r = mpatches.Rectangle((gt[0], gt[1]), gt[2], gt[3], linewidth=1, linestyle="solid",
-                                   edgecolor="y", facecolor=None, fill=None)
+            r = mpatches.Rectangle((gt[0], gt[1]), gt[2], gt[3], linewidth=1, linestyle="solid", edgecolor="y",
+                                   facecolor=None, fill=None)
             ax.add_patch(r)
         x = pf.get_x().T
         plt.gca().autoscale(False)
         if extract_density:
-            kernel = st.gaussian_kde(x)
+            if point_estimate:
+                data = x
+            else:
+                data = np.vstack((x[0] + x[2], x[1] + x[3])) // 2
+            kernel = st.gaussian_kde(data)
             positions = np.vstack([xx.ravel(), yy.ravel()])
             f = np.reshape(kernel(positions).T, xx.shape)
             imean = np.argmax(f)
             xmean = int(round(xx[imean // 100, imean % 100]))
             ymean = int(round(yy[imean // 100, imean % 100]))
         if plot_particles:
-            plt.plot(x[0].astype(int), x[1].astype(int), 'g.')
+            if point_estimate:
+                plt.plot(x[0].astype(int), x[1].astype(int), 'g.')
+            else:
+                for xi in sample(x.T.tolist(), 10):
+                    r = mpatches.Rectangle((int(xi[0]), int(xi[1])), int(abs(xi[2]-xi[0])), int(abs(xi[3]-xi[1])),
+                                           linewidth=1, linestyle="solid", edgecolor="y", facecolor=None, fill=None)
+                    ax.add_patch(r)
         if plot_density and extract_density:
             f[f < 1e-5] = np.nan
             cfset = ax.contourf(xx, yy, f, cmap='coolwarm', alpha=0.5)
@@ -141,8 +184,11 @@ def main():
             r = mpatches.Rectangle((xmean - w/2, ymean - h/2), gt[2], gt[3], linewidth=1, linestyle="solid",
                                    edgecolor="b", facecolor=None, fill=None)
             ax.add_patch(r)
-            v = np.linalg.norm([xmean - gt[0] - w/2, ymean - gt[1] - h/2], axis=0)
-            t.set_description(f"pred_pos=({xmean:3}, {ymean:3}) | gt_pos=({round(gt[0] + w/2):3}, {round(gt[1] + h/2):3}) | l2_dist={v:2.4}")
+            v = np.linalg.norm(
+                [xmean - gt[0] - w/2, ymean - gt[1] - h/2], axis=0)
+            t.set_description(
+                f"pred_pos=({xmean:3}, {ymean:3}) | gt_pos=({round(gt[0] + w/2):3}, {round(gt[1] + h/2):3}) |"
+                " l2_dist={v:2.4}")
             diff += v
         plt.pause(0.1)
 

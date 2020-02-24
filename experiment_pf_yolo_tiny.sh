be9d21862da296d04007cb6b8a668cc66e59546f
Sergio Liberman Bronfman <sergiolib@gmail.com>
2020-02-24 22:40:08 +0100

    Added results for tests with pf and full yolo

--- experiment_pf_yolo_tiny.sh
+++ experiment_pf_yolo_tiny.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-python track_pf.py --extract_density \
-                   --plot_detected_mean \
-                   --resample_mode systematic \
+python3 track_pf.py --extract_density \
+                    --resample_mode systematic \
                    --num_particles 10000 \
-                   yolo_tiny
+                    --point_estimate \
+                    yolo_tiny
+
+python3 track_pf.py --extract_density \
+                    --resample_mode systematic \
+                    --num_particles 10000 \
+                    yolo_tiny
